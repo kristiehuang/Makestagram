10307b98ee9e7deca95b71a8e4681f070785f2aa
Kristie Huang <kristiehuang01@gmail.com>
2017-06-27 13:32:17 -0700

    Add app structure

---
+++ Makestagram/View Controllers/FindFriendsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  FindFriendsViewController.swift
+//  Makestagram
+//
+//  Created by Kristie Huang on 6/27/17.
+//  Copyright © 2017 Kristie Huang. All rights reserved.
+//
+
+import Foundation
